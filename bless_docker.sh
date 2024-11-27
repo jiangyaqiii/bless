3b9ae40f24353883179a3a212ae23b488b01935f
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-11-28 01:30:00 +0800

    Update bless_docker.sh

--- bless_docker.sh
+++ bless_docker.sh
@@ -20,7 +20,7 @@ if ! command -v docker &> /dev/null; then
     sudo apt-get update
     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    echo -e "n" | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
     sudo apt-get install -y docker-ce
     echo "Docker 已安装。"
